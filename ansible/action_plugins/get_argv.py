4f1dd8589d1d05c1cc37a55e25d7c5d6668a902e
Alberto Gonzalez <alberto.gonzalez@redhat.com>
2017-10-05 16:54:11 +0200

    HSRACKDEV-205 #qa #comment configure install-host server

---
+++ ansible/action_plugins/get_argv.py
@@ -0,0 +1,9 @@
+from ansible.plugins.action import ActionBase
+import sys
+
+class ActionModule(ActionBase):
+
+    TRANSFERS_FILES = False
+
+    def run(self, tmp=None, task_vars=None):
+        return { 'changed': False, 'ansible_facts': { 'argv': ["'%s'" % arg for arg in sys.argv] + ["-e use_install_host=no"] } }
