d7a13cf47c443fd8f8f4cc010ed04e293101acc3
Alberto Gonzalez <alberto.gonzalez@redhat.com>
2017-10-06 13:39:57 +0200

    HSRACKDEV-205 HSRACKDEV-209 HSRACKDEV-206 HSRACKDEV-208 HSRACKDEV-208 #comment not good practice to commit for several tickets but i didnt have permission before

--- ansible/action_plugins/get_argv.py
+++ ansible/action_plugins/get_argv.py
@@ -6,4 +6,4 @@ class ActionModule(ActionBase):
     TRANSFERS_FILES = False
 
     def run(self, tmp=None, task_vars=None):
-        return { 'changed': False, 'ansible_facts': { 'argv': ["'%s'" % arg for arg in sys.argv] + ["-e use_install_host=no"] } }
+        return { 'changed': False, 'ansible_facts': { 'argv': ["'%s'" % arg for arg in sys.argv] + ["-e is_install_host=yes"] } }
